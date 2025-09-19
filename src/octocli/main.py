cd056363b9f10d0b19c9ed249a118c9d4d637594
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2025-09-19 16:46:46 -0300

    feat: adding the '-v' or '--version' option

--- src/octocli/main.py
+++ src/octocli/main.py
@@ -1,9 +1,33 @@
+import os
+import tomllib
+
 from rich import print
-from typer import Typer
+from typer import Exit, Option, Typer
 
 app = Typer(name="octocli")
 
 
-@app.command()
-def load():
-    print("Loading portal gun")
+def version_callback(v: bool) -> None:
+    if v:
+        path = os.getcwd()
+        with open(f"{path}/pyproject.toml", "rb") as file:
+            data = tomllib.load(file)
+            version = data.get("project", {"version": "0.1.0"}).get("version")
+            print(f"OctoCLI {version}")
+        raise Exit()
+
+
+@app.callback()
+def commom(
+    version: bool = Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+    )
+) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    app()

d09137e91389be0faa7880444cb7ca42d2b9c7ae
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2026-02-26 12:36:00 -0300

    main.py is a test file and updating the model to use pathlib

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.octocli.model import Model
+
+m = Model("user")
+
+while True:
+    char = input()
+    match char:
+        case "C" | "c":
+            m.create()
+        case "R" | "r":
+            print(m.read())
+        case "U" | "u":
+            m.update("test")
+        case "D" | "d":
+            m.delete()
+        case _:
+            break
--- src/octocli/model.py
+++ src/octocli/model.py
@@ -1,43 +1,59 @@
-import os
+from pathlib import Path
 
 
 class Model:
-    classname: str
-    tablename: str
-
     def __init__(self, tablename: str) -> None:
         self.tablename = tablename
         self.classname = tablename.title().replace("_", "")
 
-    def create(self, dest: str = os.path.abspath(os.getcwd())) -> None:
-        tpath = f"{dest}/templates/models/standard.py"
-        with open(tpath) as file:
+    def create(self, destpath: Path = Path.cwd() / "models") -> None:
+        temppath = Path(__file__).parent / "templates" / "models" / "standard.py"
+        with temppath.open() as file:
             content = file.read()
 
         content = content.replace("{{ classname }}", self.classname)
         content = content.replace("{{ tablename }}", self.tablename)
 
-        dpath = f"{dest}/{self.tablename}.py"
-        with open(dpath, "w") as file:
+        if not destpath.exists() and not destpath.name.endswith(".py"):
+            destpath.mkdir()
+
+        filepath = (
+            destpath
+            if destpath.name.endswith(".py")
+            else Path(f"{destpath}/{self.tablename}.py")
+        )
+        with open(filepath, "w") as file:
             file.write(content)
 
-    def read(self, src: str = os.path.abspath(os.getcwd())) -> str:
-        srcpath = src if src.endswith(".py") else f"{src}/{self.tablename}.py"
-        with open(srcpath, "r") as file:
+    def read(self, srcpath: Path = Path.cwd() / "models") -> str:
+        filepath = (
+            srcpath
+            if srcpath.name.endswith(".py")
+            else Path(f"{srcpath}/{self.tablename}.py")
+        )
+
+        if not filepath.exists():
+            return f"No such file or directory {filepath}"
+
+        with open(filepath, "r") as file:
             content = file.read()
 
         return content
 
-    def update(
-        self,
-        content: str,
-        mode: str = "w",
-        src: str = os.path.abspath(os.getcwd()),
-    ) -> None:
-        srcpath = src if src.endswith(".py") else f"{src}/{self.tablename}.py"
-        with open(srcpath, mode) as file:
+    def update(self, content: str, srcpath: Path = Path.cwd() / "models") -> None:
+        filepath = (
+            srcpath
+            if srcpath.name.endswith(".py")
+            else Path(f"{srcpath}/{self.tablename}.py")
+        )
+        with filepath.open("w") as file:
             file.write(content)
 
-    def delete(self, src: str = os.path.abspath(os.getcwd())) -> None:
-        srcpath = src if src.endswith(".py") else f"{src}/{self.tablename}.py"
-        os.remove(srcpath)
+    def delete(self, srcpath: Path = Path.cwd() / "models") -> None:
+        filepath = (
+            srcpath
+            if srcpath.name.endswith(".py")
+            else Path(f"{srcpath}/{self.tablename}.py")
+        )
+        if filepath.exists():
+            filepath.unlink(True)

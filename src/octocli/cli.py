8be1650b561ae7ac6a70ad1821a4778f13dfa482
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2026-03-06 12:14:30 -0300

    editing the cli to not require every command

--- src/octocli/cli.py
+++ src/octocli/cli.py
@@ -14,7 +14,7 @@ def build_parser() -> ArgumentParser:
         version=f"🐙 OctoCLI {__version__} 🐙",
     )
 
-    subparsers = parser.add_subparsers(dest="command", required=True)
+    subparsers = parser.add_subparsers(dest="command")
 
     subparsers.add_parser(
         "init",
@@ -25,37 +25,37 @@ def build_parser() -> ArgumentParser:
         "model",
         help="Manages SQLAlchemy Models on cwd",
     )
-    model_subparser = model_parser.add_subparsers(dest="action", required=True)
+    model_subparser = model_parser.add_subparsers(dest="action")
 
     model_create = model_subparser.add_parser(
         "create",
         help="Create a model following CPID standard pattern",
     )
-    model_create.add_argument("tablename", required=True, help="SQL table name")
+    model_create.add_argument("tablename", help="SQL table name")
 
     model_read = model_subparser.add_parser(
         "read",
         help="Prints the file content on stdout",
     )
-    model_read.add_argument("tablename", required=True, help="SQL table name")
+    model_read.add_argument("tablename", help="SQL table name")
 
     model_delete = model_subparser.add_parser(
         "delete",
         help="Deletes Model file of the provided tablename",
     )
-    model_delete.add_argument("tablename", required=True, help="SQL table name")
+    model_delete.add_argument("tablename", help="SQL table name")
 
     model_list = model_subparser.add_parser(
         "list",
         help="List columns of tablename on a table view",
     )
-    model_list.add_argument("tablename", required=True, help="SQL table name")
+    model_list.add_argument("tablename", help="SQL table name")
 
     model_addcol = model_subparser.add_parser(
         "add",
         help="Add a column/attribute on a Model with tablename",
     )
-    model_addcol.add_argument("tablename", required=True, help="SQL table name")
+    model_addcol.add_argument("tablename", help="SQL table name")
     model_addcol.add_argument("--name", help="Name of the column")
     model_addcol.add_argument("--type", help="Python type of the column")
 
@@ -63,7 +63,7 @@ def build_parser() -> ArgumentParser:
         "remove",
         help="Remove a column/attribute on a Model with tablename",
     )
-    model_rmcol.add_argument("tablename", required=True, help="SQL table name")
+    model_rmcol.add_argument("tablename", help="SQL table name")
     model_rmcol.add_argument("--name", help="Name of the column")
 
     return parser
